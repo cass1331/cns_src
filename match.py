77143d267e0611628d6372f31ac21cbe18f39979
Jocasta Manasseh-Lewis <jmanasse@sh03-ln06.stanford.edu>
2023-10-05 07:36:25 -0700

    update matching

--- match.py
+++ match.py
@@ -12,27 +12,7 @@ import pandas as pd
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print("device:",device)
 
-transformation = super_transformation()
-train_data = MRI_Dataset(fold = 0 , stage= 'original_train',transform = transformation)
-
-loader = DataLoader(dataset=train_data ,
-                          batch_size=len(train_data),
-                          shuffle=False,
-                          pin_memory=True,
-                          num_workers=3)
-
-images = next(iter(loader))[0].numpy()  
-labels = next(iter(loader))[1].numpy()
-ci_labels = np.array([el[0] for el in labels]) 
-hiv_labels = np.array([el[1] for el in labels])
-actual_labels = next(iter(loader))[2].numpy()
-#datasets = next(iter(vloader))[3].numpy()
-ids = np.array(next(iter(loader))[4])
-ages = next(iter(loader))[5].numpy()
-genders = next(iter(loader))[6].numpy()
-npzs = next(iter(loader))[7].numpy() 
-
-data = pd.DataFrame.from_dict({"ci_labels": ci_labels, "hiv_labels": hiv_labels, "actual_labels": actual_labels, "ids":ids, "ages": ages, "genders": genders, "npzs": npzs})
+data = pd.read_csv('/home/users/jmanasse/match_frame.csv')  
 
 def matchGroup(data, group):
     # Split data into two groups
@@ -75,7 +55,7 @@ def matchGroup(data, group):
     # Create a directed graph and find the max flow
     G = nx.DiGraph()
     G.add_edges_from(zip(s, t), capacity=weights)
-    flow_value, flow_dict = nx.maximum_flow(G, N_etoh + N_ctrl + 1, N_etoh + N_ctrl + 2)
+    flow_value, flow_dict = nx.maximum_flow(G, N_etoh + N_ctrl + 1, N_etoh + N_ctrl + 2, capacity='1.0')
 
     # Extract selected samples
     selected_etoh = [i for i in range(N_etoh) if flow_dict[i] == 1]
